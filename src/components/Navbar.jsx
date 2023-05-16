bb2f11831b2935ec3029809c84e1e678ab4cbfe1
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2023-05-16 13:34:28 -0400

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,44 @@
 import React from "react";
-import Logo from '../assets/g_logo_anuja.gif'
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/Anuja_Logo.png";
 
 export default function Navbar() {
   return (
-    <div >
-      <div className="fixed w-full h-[80px] flex-between items-center px-4 bg-[#0a192f] text-gray-400">
-        <img src={Logo} alt="data"></img>
+    <div className="fixed w-full h-[90px] flex justify-between items-center px-5 bg-[#0a192f] text-gray-400">
+      <div>
+        <img
+          src={Logo}
+          alt="data"
+          style={{ width: "180px", height: "100px" }}
+        ></img>
       </div>
+      {/* menu */}
+      <div className="flex">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Work Experinece</li>
+          <li>Skills</li>
+          <li>contact</li>
+        </ul>
+      </div>
+
+      {/* hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Work Experinece</li>
+        <li>Skills</li>
+        <li>contact</li>
+      </ul>
+
+      {/* social icons`` */}
+      <div></div>
     </div>
   );
 }
-
-
